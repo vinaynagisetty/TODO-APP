2b69bf1cdb7f6ca32211744c45e49ba0a5adc048
vinay <vinaynagisetty4229@gmail.com>
2022-12-14 19:45:47 +0530

    todo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import {Todo} from "./todo";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'TODO1';
+   
+  todoValue:string;
+  list:Todo[]=[];
+
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+  addItem(){
+    if(this.todoValue !==""){
+      const newItem:Todo={
+        value: this.todoValue,
+        isDone: false,
+        
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue="";
+  }
+
+  deleteTodo(index:number): void {
+    this.list.splice(index,1);
+  }
+
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,5 @@
+export interface Todo{
+    
+    value:String;
+    isDone:boolean;
+}
